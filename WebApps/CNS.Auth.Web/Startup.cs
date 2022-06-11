600caf944ead85d100393650106b57e58dd00fd7
Fulvio Mercoliano <fumer@microsoft.com>
2022-06-11 23:12:31 +0200

    Add an intentional error

--- WebApps/CNS.Auth.Web/Startup.cs
+++ WebApps/CNS.Auth.Web/Startup.cs
@@ -38,6 +38,7 @@ namespace CNS.Auth.Web
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			intentional error
 			services.AddApplicationInsightsTelemetry();
 			services.AddControllersWithViews().
 				AddRazorRuntimeCompilation();
