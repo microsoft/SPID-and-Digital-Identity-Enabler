fc6ac7e90e5f8466f569469aac05c0d79971189c
Fulvio Mercoliano <fumer@microsoft.com>
2023-05-19 10:04:20 +0200

    Add a configurable alteration on the SAMLResponse to change the xsi:type for the claim dateOfBirth (#59)
    * Add configurable dateOfBirth alteration
    * AlterDateOfBirth default to false in appsettings.json
    * Set default value for DateOfBirthFormat inside OptionalResponseAlterationOptions

---
+++ WebApps/Proxy/Microsoft.SPID.Proxy/Models/Options/OptionalResponseAlterationOptions.cs
@@ -0,0 +1,13 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace Microsoft.SPID.Proxy.Models.Options
+{
+	public class OptionalResponseAlterationOptions
+	{
+		public bool AlterDateOfBirth { get; set; }
+		public string DateOfBirthFormat { get; set; } = "xs:date";
+    }
+}
