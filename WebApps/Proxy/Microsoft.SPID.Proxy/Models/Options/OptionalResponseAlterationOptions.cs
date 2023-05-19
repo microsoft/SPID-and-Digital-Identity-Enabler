846e1dc19f74abf2290f0b8bf190474cf8f04266
Fulvio Mercoliano <fumer@microsoft.com>
2023-05-19 09:58:13 +0200

    Set default value for DateOfBirthFormat inside OptionalResponseAlterationOptions

--- WebApps/Proxy/Microsoft.SPID.Proxy/Models/Options/OptionalResponseAlterationOptions.cs
+++ WebApps/Proxy/Microsoft.SPID.Proxy/Models/Options/OptionalResponseAlterationOptions.cs
@@ -8,6 +8,6 @@ namespace Microsoft.SPID.Proxy.Models.Options
 	public class OptionalResponseAlterationOptions
 	{
 		public bool AlterDateOfBirth { get; set; }
-        public string DateOfBirthFormat { get; set; }
+		public string DateOfBirthFormat { get; set; } = "xs:date";
     }
 }
