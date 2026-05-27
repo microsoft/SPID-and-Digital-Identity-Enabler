b986eab1d03f2b601939d197891063f49e28ef54
Fulvio Mercoliano <fumer@microsoft.com>
2026-05-27 23:48:14 +0200

    Add SAMLResponseID and AuthnContextClassRef attributes to optional response alterations

--- WebApps/Proxy/Microsoft.SPID.Proxy/Models/LoggingEvents.cs
+++ WebApps/Proxy/Microsoft.SPID.Proxy/Models/LoggingEvents.cs
@@ -17,6 +17,8 @@ namespace Microsoft.SPID.Proxy.Models
 		public const int PROXY_INDEX_INVOKED = 5006;
 		public const int INCOMING_SAML_RESPONSE_DECODED = 5007;
 		public const int ALTERED_DATEOFBIRTH_TYPE = 5008;
+		public const int ADDED_SAMLRESPONSEID_ATTRIBUTE = 5009;
+		public const int ADDED_AUTHNCONTEXTCLASSREF_ATTRIBUTE = 5010;
 
 
 
--- WebApps/Proxy/Microsoft.SPID.Proxy/Models/Options/OptionalResponseAlterationOptions.cs
+++ WebApps/Proxy/Microsoft.SPID.Proxy/Models/Options/OptionalResponseAlterationOptions.cs
@@ -9,5 +9,13 @@ namespace Microsoft.SPID.Proxy.Models.Options
 	{
 		public bool AlterDateOfBirth { get; set; }
 		public string DateOfBirthFormat { get; set; } = "xs:date";
-    }
+
+		public bool AddSAMLResponseIDAttribute { get; set; }
+		public string SAMLResponseIDAttributeName { get; set; } = "OriginalSAMLResponseID";
+		public string SAMLResponseIDAttributeXsiType { get; set; } = "xs:string";
+
+		public bool AddAuthnContextClassRefAttribute { get; set; }
+		public string AuthnContextClassRefAttributeName { get; set; } = "SPIDLevel";
+		public string AuthnContextClassRefAttributeXsiType { get; set; } = "xs:string";
+	}
 }
